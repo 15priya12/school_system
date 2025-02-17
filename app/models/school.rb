f43fc9ad984db026aa1ffe361a5624066878b0c1
priya <15priya12jain@gmail.com>
2025-02-17 22:20:17 +0530

    manually creating tenants now

--- app/models/school.rb
+++ app/models/school.rb
@@ -10,9 +10,25 @@ class School < ApplicationRecord
     private
   
     def create_tenant
-        return if username.blank?  # Prevents Apartment from running if username is nil or empty
-        Apartment::Tenant.create(username)
+      return if username.blank?  # Prevents running if username is nil or empty
+    
+      db_name = username # Define database name convention
+    
+      begin
+        ActiveRecord::Base.connection.execute("CREATE DATABASE #{db_name}") # Create database
+        ActiveRecord::Base.connection.execute("USE #{db_name}") # Switch to that database
+        ActiveRecord::Base.connection.execute("CREATE TABLE students (
+          id INT AUTO_INCREMENT PRIMARY KEY,
+          email VARCHAR(255) UNIQUE,
+          password_digest VARCHAR(255) ,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        )") # Create students table
+      rescue StandardError => e
+        Rails.logger.error "Error creating tenant database: #{e.message}"
       end
+    end
+    
       
   end
   
